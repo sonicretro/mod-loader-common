1954c07013618e9ace840bbdae006b647b85768a
Michael Fadely <michaelwfadely@gmail.com>
2019-11-24 17:15:14 -0700

    some initial implementations for metadata

---
+++ ModManagerCommon/PackageDefinition.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModManagerCommon
+{
+	public class PackageBase : IEquatable<PackageBase>, IComparable<PackageBase>
+	{
+		/// <summary>
+		/// The name of the package.
+		/// </summary>
+		public readonly string Name;
+
+		/// <summary>
+		/// The version of the package.
+		/// </summary>
+		public readonly SemVer Version;
+
+		public PackageBase(string name, SemVer version)
+		{
+			Name    = name;
+			Version = version;
+		}
+
+		/// <inheritdoc />
+		public bool Equals(PackageBase other)
+		{
+			if (other is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			return Name == other.Name && Version.Equals(other.Version);
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj)
+		{
+			if (obj is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			if (obj.GetType() != GetType())
+			{
+				return false;
+			}
+
+			return Equals((PackageBase)obj);
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (Name.GetHashCode() * 397) ^ Version.GetHashCode();
+			}
+		}
+
+		/// <inheritdoc />
+		public int CompareTo(PackageBase other)
+		{
+			if (ReferenceEquals(this, other))
+			{
+				return 0;
+			}
+
+			if (other is null)
+			{
+				return 1;
+			}
+
+			int nameComparison = string.Compare(Name, other.Name, StringComparison.Ordinal);
+
+			if (nameComparison != 0)
+			{
+				return nameComparison;
+			}
+
+			return Comparer<SemVer>.Default.Compare(Version, other.Version);
+		}
+	}
+
+	public class PackageDefinition : PackageBase, IEquatable<PackageDefinition>
+	{
+		/// <summary>
+		/// The authors that contributed to this package.
+		/// </summary>
+		public readonly string[] Authors;
+
+		/// <summary>
+		/// Packages which this package depends on.
+		/// The key is the package name, and the value is the package version.
+		///
+		/// Package versions starting with '~' indicate that the version must be
+		/// >= the specified version.
+		///
+		/// Otherwise, the package version is treated as an exact match.
+		/// </summary>
+		public readonly Dictionary<string, string> Dependencies;
+
+		public PackageDefinition(string name, SemVer version, string[] authors = null, Dictionary<string, string> dependencies = null)
+			: base(name, version)
+		{
+			Authors      = authors ?? new string[0];
+			Dependencies = dependencies ?? new Dictionary<string, string>();
+		}
+
+		/// <inheritdoc />
+		public bool Equals(PackageDefinition other)
+		{
+			if (other is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			return base.Equals(other) && Equals(Authors, other.Authors) && Equals(Dependencies, other.Dependencies);
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj)
+		{
+			if (obj is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			if (obj.GetType() != GetType())
+			{
+				return false;
+			}
+
+			return Equals((PackageDefinition)obj);
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hashCode = base.GetHashCode();
+				hashCode = (hashCode * 397) ^ (Authors != null ? Authors.GetHashCode() : 0);
+				hashCode = (hashCode * 397) ^ (Dependencies != null ? Dependencies.GetHashCode() : 0);
+				return hashCode;
+			}
+		}
+	}
+
+	public class InstalledPackage : PackageBase, IEquatable<InstalledPackage>
+	{
+		/// <summary>
+		/// Indicates that this package is installed exclusively as a dependency.
+		/// </summary>
+		public readonly bool AsDependency;
+
+		public InstalledPackage(string name, SemVer version, bool asDependency)
+			: base(name, version)
+		{
+			AsDependency = asDependency;
+		}
+
+		/// <inheritdoc />
+		public bool Equals(InstalledPackage other)
+		{
+			if (other is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			return base.Equals(other) && AsDependency == other.AsDependency;
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj)
+		{
+			if (obj is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			if (obj.GetType() != GetType())
+			{
+				return false;
+			}
+
+			return Equals((InstalledPackage)obj);
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (base.GetHashCode() * 397) ^ AsDependency.GetHashCode();
+			}
+		}
+	}
+}
---
+++ ModManagerCommon/SemVer.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ModManagerCommon
+{
+	/// <summary>
+	/// SemVer 2.0.0-compliant class for version comparison.
+	/// https://semver.org/
+	/// </summary>
+	public class SemVer : IComparable<SemVer>, IEquatable<SemVer>
+	{
+		static readonly Regex regex = new Regex(@"^(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$");
+
+		public readonly long   Major;
+		public readonly long   Minor;
+		public readonly long   Patch;
+		public readonly string PreRelease;
+		public readonly string BuildMetaData;
+
+		public SemVer(string versionString)
+		{
+			Match match = regex.Match(versionString);
+			Trace.Assert(match.Success);
+
+			Major         = long.Parse(match.Groups["major"].Value);
+			Minor         = long.Parse(match.Groups["minor"].Value);
+			Patch         = long.Parse(match.Groups["patch"].Value);
+			PreRelease    = match.Groups["prerelease"].Value;
+			BuildMetaData = match.Groups["buildmetadata"].Value;
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+
+			sb.Append($"{Major}.{Minor}.{Patch}");
+
+			if (!string.IsNullOrEmpty(PreRelease))
+			{
+				sb.Append($"-{PreRelease}");
+			}
+
+			if (!string.IsNullOrEmpty(BuildMetaData))
+			{
+				sb.Append($"+{BuildMetaData}");
+			}
+
+			return sb.ToString();
+		}
+
+		/// <inheritdoc />
+		public int CompareTo(SemVer other)
+		{
+			int comparison = Major.CompareTo(other.Major);
+
+			if (comparison != 0)
+			{
+				return comparison;
+			}
+
+			comparison = Minor.CompareTo(other.Minor);
+
+			if (comparison != 0)
+			{
+				return comparison;
+			}
+
+			comparison = Patch.CompareTo(other.Patch);
+
+			if (comparison != 0)
+			{
+				return comparison;
+			}
+
+			bool thisPreReleaseNull = string.IsNullOrEmpty(PreRelease);
+			bool thatPreReleaseNull = string.IsNullOrEmpty(other.PreRelease);
+
+			if (thisPreReleaseNull && !thatPreReleaseNull)
+			{
+				return -1;
+			}
+
+			if (thatPreReleaseNull && !thisPreReleaseNull)
+			{
+				return 1;
+			}
+
+			return string.Compare(PreRelease, other.PreRelease, StringComparison.Ordinal);
+		}
+		
+		/// <inheritdoc />
+		public bool Equals(SemVer other)
+		{
+			if (other is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			return Major == other.Major &&
+			       Minor == other.Minor &&
+			       Patch == other.Patch &&
+			       PreRelease == other.PreRelease &&
+			       BuildMetaData == other.BuildMetaData;
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hashCode = Major.GetHashCode();
+				hashCode = (hashCode * 397) ^ Minor.GetHashCode();
+				hashCode = (hashCode * 397) ^ Patch.GetHashCode();
+				hashCode = (hashCode * 397) ^ PreRelease.GetHashCode();
+				hashCode = (hashCode * 397) ^ BuildMetaData.GetHashCode();
+				return hashCode;
+			}
+		}
+	}
+
+	public class SemVerComparer : IComparer<SemVer>
+	{
+		/// <inheritdoc />
+		public int Compare(SemVer x, SemVer y)
+		{
+			switch (x)
+			{
+				case null when y is null:
+					return 0;
+				case null:
+					return 1;
+			}
+
+			if (y is null)
+			{
+				return -1;
+			}
+
+			if (x.Major == y.Major &&
+			    x.Minor == y.Minor &&
+			    x.Patch == y.Patch &&
+			    x.PreRelease == y.PreRelease)
+			{
+				return 0;
+			}
+
+			return x.CompareTo(y);
+		}
+	}
+}
