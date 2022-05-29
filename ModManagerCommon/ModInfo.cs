17e635ba63f366121e4443a289e84493e440eb04
PiKeyAr <PiKeyAr@users.noreply.github.com>
2022-05-29 16:54:55 +0900

    Add mod category to ModInfo class

--- ModManagerCommon/ModInfo.cs
+++ ModManagerCommon/ModInfo.cs
@@ -6,6 +6,7 @@ namespace ModManagerCommon
 	public class ModInfo
 	{
 		public string Name { get; set; }
+		public string Category { get; set; }
 		public string Author { get; set; }
 		public string Version { get; set; }
 		public string Description { get; set; }
